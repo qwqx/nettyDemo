290185e46a4490faf557f1b4b7d934c2c418a414
qwqx <1247931064@qq.com>
2018-12-19 09:46:38 +0800

    netty first

---
+++ src/main/java/NettyServer.java
@@ -0,0 +1,40 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        System.out.println();
+                    }
+                });
+
+        //serverBootstrap.bind(8000);
+        bind(serverBootstrap, 8000);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功!");
+                } else {
+                    System.err.println("端口[" + port + "]绑定失败!");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+    }
+}
